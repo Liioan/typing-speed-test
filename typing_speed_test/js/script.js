bd4ef9ddd44263555eb50791f7d8eda6ec5acddc
David Bijok <106277069+Liioan@users.noreply.github.com>
2022-09-21 21:11:20 +0200

    Add files via upload

---
+++ typing_speed_test/js/script.js
@@ -0,0 +1,114 @@
+const input = document.querySelector('#user-text')
+const songText = document.querySelector('.lyrics');
+const textWrapper = document.querySelector('.text-wrapper');
+const titleBand = document.querySelector('.title-band');
+const restartBtn = document.querySelector('.restart-btn');
+
+let timer;
+let time = 30;
+let timeLeft = time;
+let charIndex = 0;
+let isTyping = 0;
+let mistakes = 0;
+let currWpm = 0;
+
+const loadLyrics = () => {
+    const randomSong = Math.floor(Math.random() * songs.length);
+    // console.log(randomSong);
+    songText.innerHTML = '';
+    songs[randomSong].lyrics.split("").forEach(char => {
+        songText.innerHTML += `<span>${char}</span>`;
+    });
+    titleBand.textContent = `${songs[randomSong].songName} - ${songs[randomSong].artist}`;
+    songText.querySelectorAll('span')[0].classList.add('active');
+    document.addEventListener("keydown", () => input.focus());
+    songText.addEventListener("click", () => input.focus());
+};
+
+
+const startTimer = () => {
+    if(timeLeft > 0) {
+        timeLeft--;
+        wpm = Math.round(((charIndex - mistakes)  / 5) / (time - timeLeft) * 30);
+        currWpm = wpm
+     } else{
+        clearInterval(timer);
+    }
+};
+
+const saveWpm = (wpm) =>{
+    localStorage.setItem('lastWpm', JSON.stringify(wpm));
+}
+
+const showEndScreen = wpm => {
+    input.style.display = 'none'
+    if(localStorage.getItem('lastWpm')){
+        let lastWpm = JSON.parse(localStorage.getItem('lastWpm'));
+        songText.innerHTML = `
+        <h2 class='score'>last wpm: ${JSON.parse(localStorage.getItem('lastWpm'))}</h2>
+        <h2 class='score'>wpm: ${wpm}, time: 30s, errors: ${mistakes}</h2>
+        `;
+        if(lastWpm < wpm){
+            songText.innerHTML += `
+            <h2 class='better-score'>Your speed improved by ${wpm - lastWpm} words per minute</h2>
+            <h2 class='better-score'>Keep it up!</h2>
+            `
+        } else if(wpm < lastWpm){
+            songText.innerHTML += `
+            <h2 class='worse-score'>Your speed decreased by ${lastWpm - wpm} words per minute</h2>
+            <h2 class='worse-score'>Don't worry! Try again!</h2>
+            `
+        }
+    } else {
+        songText.innerHTML = `
+        <h2 class='score'>wpm: ${wpm}, time: 30s, errors: ${mistakes}</h2>
+        `;
+    }
+    saveWpm(wpm);
+}
+
+const typingGame = () => {
+    let allChars = songText.querySelectorAll("span");
+    let typedChar = input.value.split("")[charIndex];
+    let wpm;
+    if(charIndex < allChars.length - 1 && timeLeft > 0) {
+        if(!isTyping) {
+            timer = setInterval(startTimer, 1000);
+            isTyping = true;
+        }
+        if(typedChar == null) {
+            if(charIndex > 0) {
+                charIndex--;
+                if(allChars[charIndex].classList.contains("incorrect")) {
+                    mistakes--;
+                }
+                allChars[charIndex].classList.remove("correct", "incorrect");
+            }
+        } else {
+            if(allChars[charIndex].innerText == typedChar) {
+                allChars[charIndex].classList.add("correct");
+            } else {
+                mistakes++;
+                allChars[charIndex].classList.add("incorrect");
+            }
+            charIndex++;
+        }
+        allChars.forEach(span => span.classList.remove("active"));
+        allChars[charIndex].classList.add("active");
+
+        wpm = Math.round(((charIndex - mistakes)  / 5) / (time - timeLeft) * 30);
+        wpm = wpm < 0 || !wpm || wpm === Infinity ? 0 : wpm;        
+    } else {
+        clearInterval(timer);
+        input.value = "";
+        showEndScreen(currWpm);
+    }   
+};
+
+
+restartBtn.addEventListener('click', () => {
+    location.reload();
+});
+
+loadLyrics();
+input.addEventListener('input', typingGame);
